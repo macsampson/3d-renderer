1dec618bd5f0c07b1804c7ab6fcca401b3d04e7e
Mackenzie Sampson <mackenzie.sampson@outlook.com>
2026-04-20 18:05:39 -0700

    filled triangles

--- src/triangle.h
+++ src/triangle.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "vector.h"
 #define TRIANGLE_H
+
+#include "vector.h"
+#include <stdint.h>
 
 typedef struct {
     int a;
@@ -11,3 +13,9 @@ typedef struct {
 typedef struct {
     vec2_t points[3];
 } triangle_t;
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+
+void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int Mx, int My, uint32_t color);
+
+void fill_flat_top_triangle(int x1, int y1, int Mx, int My, int x2, int y2, uint32_t color);
